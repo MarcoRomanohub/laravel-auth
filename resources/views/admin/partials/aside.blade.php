f5ee2f87117a633e8685e7b9e2ecd531f705a275
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-22 21:55:02 +0200

    [add] admin home css

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,30 @@
+<aside class="bg-dark navbar-dark text-white ">
+    <nav>
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fa-solid fa-diagram-project"></i>
+                    Progetti
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-solid fa-plus"></i>
+                    Nuovo Progetto
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-brands fa-codepen"></i>
+                    Tecnologie
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-solid fa-list"></i>
+                    Tipologie
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,20 +1,17 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar-nav navbar-dark bg-dark">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+
             <div class="collapse navbar-collapse d-flex justify-content-between " id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                <ul class="navbar-nav me-auto mb-lg-0">
+                    {{-- <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="{{ route('admin.home') }}">Home</a>
-                    </li>
+                    </li> --}}
                     <li class="nav-item">
                         <a class="nav-link" target="_blank" href="{{ route('home') }}">Vai al sito</a>
                     </li>
                 </ul>
-                <ul class="navbar-nav mb-2 mb-lg-0">
+                <ul class="navbar-nav text-white  mb-lg-0">
                     <li>
                         {{ Auth::user()->name }}
                     </li>
